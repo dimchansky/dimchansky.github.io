5837bce6012da58371359b67c4ee1d57b344f521
Dmitrij Koniajev <dimchansky@gmail.com>
2014-10-07 02:48:59 +0300

    Design change, supporting tags and description

--- site.hs
+++ site.hs
@@ -1,5 +1,6 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+import           Data.Functor ((<$>))
 import           Data.Monoid (mappend)
 import           Hakyll
 import           GHC.IO.Encoding
@@ -14,10 +15,14 @@ main = do
 
 runHakyll :: IO ()
 runHakyll = hakyll $ do
+    -- Build tags
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
 
+    -- Compress CSS
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
@@ -31,39 +36,53 @@ runHakyll = hakyll $ do
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/post.html"    (tagsCtx tags)
+            >>= loadAndApplyTemplate "templates/default.html" (tagsCtx tags)
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
+    -- Render posts list
+    create ["posts.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
+            posts <- loadAll "posts/*"
+            sorted <- recentFirst posts
+            itemTpl <- loadBody "templates/postitem.html"
+            list <- applyTemplateList itemTpl postCtx sorted
+            makeItem list
+                >>= loadAndApplyTemplate "templates/posts.html" allPostsCtx
+                >>= loadAndApplyTemplate "templates/default.html" allPostsCtx
+                >>= relativizeUrls			
+	
+	-- Index
+    create ["index.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- loadAll "posts/*"
+            sorted <- take 10 <$> recentFirst posts
+            itemTpl <- loadBody "templates/postitem.html"
+            list <- applyTemplateList itemTpl postCtx sorted
+            makeItem list
+                >>= loadAndApplyTemplate "templates/index.html" (homeCtx tags list)
+                >>= loadAndApplyTemplate "templates/default.html" (homeCtx tags list)
+                >>= relativizeUrls
+				
+				
+    -- Post tags
+    tagsRules tags $ \tag pattern -> do
+        let title = "Заметки помеченные " ++ tag
+        route idRoute
+        compile $ do
+            list <- postList tags pattern recentFirst
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/posts.html"
+                        (constField "title" title `mappend`
+                            constField "body" list `mappend`
+                            defaultContext)
+                >>= loadAndApplyTemplate "templates/default.html"
+                        (constField "title" title `mappend`
+                            defaultContext)
                 >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
+				
     match "templates/*" $ compile templateCompiler
 
 
@@ -72,3 +91,30 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+tagsCtx :: Tags -> Context String
+tagsCtx tags =
+    tagsField "prettytags" tags `mappend`
+    postCtx	
+	
+allPostsCtx :: Context String
+allPostsCtx =
+    constField "title" "Все заметки" `mappend`
+    postCtx	
+	
+homeCtx :: Tags -> String -> Context String
+homeCtx tags list =
+    constField "posts" list `mappend`
+    constField "title" "Index" `mappend`
+    field "taglist" (\_ -> renderTagList tags) `mappend`
+    defaultContext
+
+postList :: Tags
+         -> Pattern
+         -> ([Item String] -> Compiler [Item String])
+         -> Compiler String
+postList tags pattern preprocess' = do
+    postItemTpl <- loadBody "templates/postitem.html"
+    posts <- loadAll pattern
+    processed <- preprocess' posts
+    applyTemplateList postItemTpl (tagsCtx tags) processed	
